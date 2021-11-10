cf013d5c0b2db5a5f72f09d0d66a4d9666e20ebf
Arun Kumar Verma <arunkumarverma22022000@gmail.com>
2021-11-10 16:55:39 +0530

    updated UI for client

--- client.java
+++ client.java
@@ -1,5 +1,5 @@
-import java.io.*;
-import java.net.*;
+import java.io.*; // Imported because we need the InputStream and OuputStream classes
+import java.net.*; // Imported because the Socket class is needed
 
 class UDPChatClient {
     public static DatagramSocket clientsocket;
@@ -8,25 +8,45 @@ class UDPChatClient {
     public static InetAddress ia;
     public static byte buf[] = new byte[1024];
     public static int cport = 789, sport = 790;
+    // Defining colors
+    public static final String CLEAR = "\033[H\033[2J"; // CLEAR_CONSOLE
+    public static final String RED = "\033[0;31m"; // RED
+    public static final String GREEN = "\033[0;32m"; // GREEN
+    public static final String RED_BOLD = "\033[1;31m"; // RED_BOLD
+    public static final String GREEN_BOLD = "\033[1;32m"; // GREEN_BOLD
+    public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW_BOLD
+    public static final String ANSI_RESET = "\u001B[0m"; // ANSI_RESET
+
+    public static void printMessage(String preS, String ANSI_COLOR, String message) {
+        System.out.print(ANSI_COLOR + preS + ": " + ANSI_RESET + message);
+    }
+
+    public static void printText(String ANSI_COLOR, String message) {
+        System.out.print(ANSI_COLOR + message + ANSI_RESET);
+    }
 
     public static void main(String[] a) throws IOException {
         clientsocket = new DatagramSocket(cport);
         dp = new DatagramPacket(buf, buf.length);
         dis = new BufferedReader(new InputStreamReader(System.in));
         ia = InetAddress.getLocalHost();
-        System.out.println("Client is Running... Type 'STOP'to Quit");
+
+        System.out.print(CLEAR);
+        printText(GREEN, "Client is Running... ");
+        printText(RED, "(Type 'STOP'to Quit) \n\n");
         while (true) {
+            printMessage("Client", YELLOW_BOLD, "");
             String str = new String(dis.readLine());
             buf = str.getBytes();
             if (str.equals("STOP")) {
-                System.out.println("Terminated...");
+                printText(RED, "Terminated...");
                 clientsocket.send(new DatagramPacket(buf, str.length(), ia, sport));
                 break;
             }
             clientsocket.send(new DatagramPacket(buf, str.length(), ia, sport));
             clientsocket.receive(dp);
             String str2 = new String(dp.getData(), 0, dp.getLength());
-            System.out.println("Server: " + str2);
+            printMessage("Server", YELLOW_BOLD, str2 + "\n");
         }
     }
 }
--- server.java
+++ server.java
@@ -9,11 +9,29 @@ class UDPChatServer {
     public static byte buf[] = new byte[1024];
     public static int cport = 789, sport = 790;
 
+    // Defining colors
+    public static final String CLEAR = "\033[H\033[2J"; // CLEAR_CONSOLE
+    public static final String RED = "\033[0;31m"; // RED
+    public static final String GREEN = "\033[0;32m"; // GREEN
+    public static final String RED_BOLD = "\033[1;31m"; // RED_BOLD
+    public static final String GREEN_BOLD = "\033[1;32m"; // GREEN_BOLD
+    public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW_BOLD
+    public static final String ANSI_RESET = "\u001B[0m"; // ANSI_RESET
+
+    public static void printMessage(String preS, String ANSI_COLOR, String message) {
+        System.out.print(ANSI_COLOR + preS + ": " + ANSI_RESET + message);
+    }
+
+    public static void printText(String ANSI_COLOR, String message) {
+        System.out.print(ANSI_COLOR + message + ANSI_RESET);
+    }
+
     public static void main(String[] a) throws IOException {
         serversocket = new DatagramSocket(sport);
         dp = new DatagramPacket(buf, buf.length);
         dis = new BufferedReader(new InputStreamReader(System.in));
         ia = InetAddress.getLocalHost();
+        System.out.println(CLEAR);
         System.out.println("Server is Running...");
         while (true) {
             serversocket.receive(dp);
